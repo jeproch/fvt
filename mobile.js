22204eaee104a34b10ade366a60e35a6c56250c1
jeproch <jprevenant@gmail.com>
2023-07-09 14:06:45 -0700

    Mobile adjustments

--- mobile.js
+++ mobile.js
@@ -1,13 +1,15 @@
-let menuBtn = document.getElementById("menu-btn");
+const menuBtn = document.getElementById("menu-btn");
 const dropdownEl = document.getElementById("navigation-dropdown");
 const homePage = document.getElementById("home-page");
 const navDropdown = document.getElementById("navigation-dropdown");
 const headingContents = document.getElementById("heading-contents");
 const closeBtn = document.getElementById("close-button");
+const homeContents = document.getElementById("home-contents");
 
 document.addEventListener("DOMContentLoaded", function () {
   dropdownEl.classList.add("hide");
   navDropdown.classList.add("hide");
+  homeContents.classList.remove("hide");
 });
 
 menuBtn.addEventListener("click", function () {
@@ -15,6 +17,7 @@ menuBtn.addEventListener("click", function () {
   navDropdown.classList.remove("hide");
   menuBtn.classList.add("hide");
   headingContents.classList.add("hide");
+  homeContents.classList.add("hide");
 });
 
 closeBtn.addEventListener("click", function () {
@@ -22,4 +25,5 @@ closeBtn.addEventListener("click", function () {
   navDropdown.classList.add("hide");
   menuBtn.classList.remove("hide");
   headingContents.classList.remove("hide");
+  homeContents.classList.remove("hide");
 });
